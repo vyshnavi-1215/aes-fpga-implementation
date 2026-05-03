754c023d1a00a1be5e0a377578fb830b2a464139
vyshnavi-1215 <vyshnavimannem657@gmail.com>
2026-05-03 17:50:43 +0530

    subbytes.v

--- src/subbytes.v
+++ src/subbytes.v
@@ -1,1 +1,19 @@
 
+
+module subbytes  (
+    input  [127:0] state_in,
+    output [127:0] state_out
+);
+
+genvar i;
+
+generate
+    for (i = 0; i < 16; i = i + 1) begin : sbox_instances
+        sbox_lut sbox_inst (
+            .in (state_in[8*i +: 8]),
+            .out(state_out[8*i +: 8])
+        );
+    end
+endgenerate
+
+endmodule
